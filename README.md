9fdb75a93c3b2ca79f1b60fc2a262983f3e22741
Philipp Reiter <p.reiter93@gmail.con>
2024-12-29 15:01:11 +0100

    Add readme

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # tui-event-controller
+
+## Description
+
+A event controller for ratatui apps.
+
+## Example
+
+```rust
+use tui_event_controller::EventfulWidget;
+use ratatui::prelude::*;
+use ratatui::widgets::WidgetRef;
+
+type EventContext<'a> = tui_event_controller::EventContext<'a, AppState, AppEvent>;
+type EventController = tui_event_controller::EventController<AppState, AppEvent>;
+type IWidget<W> = tui_event_controller::InteractiveWidget<AppState, AppEvent, W>;
+
+enum AppEvent {
+    Tick,
+}
+
+struct AppState {
+    counter: usize,
+}
+
+#[derive(Default)]
+struct HomePage;
+
+impl WidgetRef for HomePage {
+    fn render_ref(&self, _area: Rect, _buf: &mut Buffer) {}
+}
+
+impl EventfulWidget<AppState, AppEvent> for HomePage {
+    fn unique_key() -> String {
+        String::from("HomePage")
+    }
+
+    fn on_event(ctx: EventContext, state: &mut AppState, _: Option<Rect>) {
+        match ctx.event {
+            AppEvent::Tick => {
+                state.counter += 1;
+                println!("HomePage: tick {:}", state.counter);
+            }
+        }
+    }
+}
+
+let controller = EventController::new();
+let home_page = IWidget::new(HomePage::default(), &controller);
+```
+
+License: MIT
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,51 @@
+//! # Description
+//!
+//! A event controller for ratatui apps.
+//!
+//! # Example
+//!
+//! ```
+//! use tui_event_controller::EventfulWidget;
+//! use ratatui::prelude::*;
+//! use ratatui::widgets::WidgetRef;
+//!
+//! type EventContext<'a> = tui_event_controller::EventContext<'a, AppState, AppEvent>;
+//! type EventController = tui_event_controller::EventController<AppState, AppEvent>;
+//! type IWidget<W> = tui_event_controller::InteractiveWidget<AppState, AppEvent, W>;
+//!
+//! enum AppEvent {
+//!     Tick,
+//! }
+//!
+//! struct AppState {
+//!     counter: usize,
+//! }
+//!
+//! #[derive(Default)]
+//! struct HomePage;
+//!
+//! impl WidgetRef for HomePage {
+//!     fn render_ref(&self, _area: Rect, _buf: &mut Buffer) {}
+//! }
+//!
+//! impl EventfulWidget<AppState, AppEvent> for HomePage {
+//!     fn unique_key() -> String {
+//!         String::from("HomePage")
+//!     }
+//!
+//!     fn on_event(ctx: EventContext, state: &mut AppState, _: Option<Rect>) {
+//!         match ctx.event {
+//!             AppEvent::Tick => {
+//!                 state.counter += 1;
+//!                 println!("HomePage: tick {:}", state.counter);
+//!             }
+//!         }
+//!     }
+//! }
+//!
+//! let controller = EventController::new();
+//! let home_page = IWidget::new(HomePage::default(), &controller);
+//! ```
 mod controller;
 mod widget;
 
